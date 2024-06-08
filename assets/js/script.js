ab43ff7f84f7ab70acfd163b73c9eea99b7cee2c
spl-RerreRojas <renata.rojas07@gmail.com>
2024-06-07 22:38:06 -0400

    Carlos sos un grande

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+const reproducirVideo = (() => {
+  const modificarUrl = (url, id) => {
+    const etiquetaElegida = document.getElementById(id);
+    etiquetaElegida.setAttribute("src", url);
+  };
+  return {
+    insertarVideo: (url, id) => {
+      modificarUrl(url, id);
+    },
+  };
+})();
+
 class Multimedia {
   constructor(url) {
     this._url = url;
@@ -17,40 +29,34 @@ class Reproductor extends Multimedia {
     super(url);
     this._id = id;
   }
-  setInicio(time) {
-    
-    const iframe = document.getElementById(id);
-    iframe.setAttribute("src", `${this._url}?start=${time}`);
-}
+  get id() {
+    return this._id;
+  }
   playMultimedia() {
-    this._url, this._id;
+    reproducirVideo.insertarVideo(this._url, this._id);
+  }
+  setInicio(time) {
+    reproducirVideo.insertarVideo(`${this._url}&start=${time}`, this._id);
   }
 }
 
-const reproducirVideo = (() => {
+const musicaPlayer = new Reproductor(
+  "https://www.youtube.com/embed/9Wz3J1P5HZY?si=kAg9abLcjZcS12kr",
+  "musica"
+);
+const peliculaPlayer = new Reproductor(
+  "https://www.youtube.com/embed/0vCaRgK_I3I?si=ETeDOiHnAkGimmD0",
+  "peliculas"
+);
+const seriePlayer = new Reproductor(
+  "https://www.youtube.com/embed/5dkXNhJ-F-c?si=kKNZNwYu535rOAA4",
+  "series"
+);
 
+//reproducirVideo.insertarVideo(musicaPlayer._url, musicaPlayer._id);
+//reproducirVideo.insertarVideo(peliculaPlayer._url, peliculaPlayer._id);
+//reproducirVideo.insertarVideo(seriePlayer._url, seriePlayer._id);
 
-  const modificarUrl = (url, id) => {
-
-    const etiquetaElegida = document.getElementById(id);
-    etiquetaElegida.setAttribute("src", url)
-
-  };
-  return {
-    insertarVideo: (url, id) => {
-      modificarUrl(url, id);
-    },
-  };
-})();
-
-const musicaPlayer = new Reproductor("https://www.youtube.com/embed/9Wz3J1P5HZY?si=kAg9abLcjZcS12kr", "musica");
-const peliculaPlayer = new Reproductor("https://www.youtube.com/embed/0vCaRgK_I3I?si=ETeDOiHnAkGimmD0", "peliculas");
-const seriePlayer = new Reproductor("https://www.youtube.com/embed/5dkXNhJ-F-c?si=kKNZNwYu535rOAA4", "series");
-
-reproducirVideo.insertarVideo(musicaPlayer._url, musicaPlayer._id)
-reproducirVideo.insertarVideo(peliculaPlayer._url, peliculaPlayer._id)
-reproducirVideo.insertarVideo(seriePlayer._url, seriePlayer._id)
-
-//musicaPlayer.setInicio(30)
-peliculaPlayer.setInicio(10)
-
+musicaPlayer.setInicio(30);
+peliculaPlayer.setInicio(10);
+seriePlayer.playMultimedia();
